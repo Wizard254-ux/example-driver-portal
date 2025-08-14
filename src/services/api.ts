34e57da133fdf76218705180b77d2afc49ffc290
Brian Albert <150579230+Wizard254-ux@users.noreply.github.com>
2025-08-14 22:32:27 +0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create axios instance with a fallback baseURL
 const api = axios.create({
-  baseURL: 'http://a23db48ead06.ngrok-free.app',
+  baseURL: 'https://a23db48ead06.ngrok-free.app',
   timeout: 10000000,
   withCredentials:true
 });
